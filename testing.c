e43fef25d789f13f0531f4c7cddc80703e1edfaa
Diseye2006 <141243378+Diseye2006@users.noreply.github.com>
2023-08-14 13:43:10 +0100

    Add files via upload

---
+++ testing.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+
+int main(){
+
+    int tries= 3;
+    char Guesswords [] [200] ={
+        'apple'
+        'banana'
+        'pineapple'
+        'ambulance'
+        'joker'
+        'future'
+        'hospital'
+        'hug'
+        'Hilter'
+    };
+
+
+
+    return 0;
+}
